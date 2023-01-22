ead2cee190ea32d3f78c4b4f917aa3e1c153abc7
cloud <cloudintheskyoutlook.es>
2023-01-22 11:40:36 -0500

    BrowserRouter changed to HashRouter

--- src/components/Rutas.tsx
+++ src/components/Rutas.tsx
@@ -1,17 +1,17 @@
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, HashRouter} from 'react-router-dom';
 import App from '../App';
 import { Profile } from './Profile';
 
 
 const Rutas = ()=>{
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<App/>}/>
                 <Route path='/profile' element={<Profile/>}/>
                 <Route path="*" element={<p>ZA wurodo</p>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
