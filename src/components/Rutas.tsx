c48405444e0704cf0a9479bdec737df1329af4cc
cloud <cloudintheskyoutlook.es>
2023-01-22 12:28:18 -0500

    package json modified

--- src/components/Rutas.tsx
+++ src/components/Rutas.tsx
@@ -6,7 +6,7 @@ import { Profile } from './Profile';
 const Rutas = ()=>{
     return(
             <Routes>
-                <Route path='/Pintash' element={<App/>}/>
+                <Route path='/' element={<App/>}/>
                 <Route path='/profile' element={<Profile/>}/>
                 <Route path="*" element={<p>ZA wurodo</p>}/>
             </Routes>
