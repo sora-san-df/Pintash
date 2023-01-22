68cce93f60c1f53207f5fbe00645f4e886e25600
cloud <cloudintheskyoutlook.es>
2023-01-22 11:48:47 -0500

    package json modified

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Rutas } from './components/Rutas'
 import App from './App'
-
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-
+    <HashRouter>
     <Rutas/>
-
+    </HashRouter>
     
   </React.StrictMode>,
 )
