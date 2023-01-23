ec2aef6bfd605e1b87a2ddb445ddb5eed0712515
cloud <cloudintheskyoutlook.es>
2023-01-23 13:35:00 -0500

    vite.config modified to see if gh pages works

---
+++ src/utils/hashGen.ts
@@ -0,0 +1,3 @@
+const hashGen = Math.floor(Math.random()*90000) + 10000; 
+
+export{hashGen}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-/** @type {import('vite').UserConfig} */
+import {hashGen} from './src/utils/hashGen'; 
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -6,5 +6,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pintash/'
+  base: '/Pintash/',
+  build:{
+    rollupOptions:{
+      output:{
+        entryFileNames: `[name]` + hashGen + `.js`,
+        chunkFileNames: `[name]` + hashGen + `.js`,
+        assetFileNames: `[name]` + hashGen + `.[ext]`
+      }
+    }
+  }
 })
